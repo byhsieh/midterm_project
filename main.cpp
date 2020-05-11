ba294ad689da133ed2b8ea77299fe113f4cdd2e3
byhsieh <61614858+byhsieh@users.noreply.github.com>
2020-05-12 02:49:40 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include "magic_wand_model_data.h"
 #include "mbed.h"
 #include "DA7212.h"
+#include <cmath>
+#include <iostream>
+#include <stdio.h>
 
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/micro/kernels/micro_ops.h"
@@ -12,6 +15,9 @@
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
 #include "uLCD_4DGL.h"
+
+#define bufferLength (32)
+#define signalLength (540)
 
 
 DA7212 audio;
@@ -32,14 +38,14 @@ Timer timers;
 Timer timer2;
 Timer timer3;
 
-
-
-int PredictGesture(float* output);
 EventQueue queue1(32 * EVENTS_EVENT_SIZE);
 EventQueue queue2(32 * EVENTS_EVENT_SIZE);
 EventQueue queue3(32 * EVENTS_EVENT_SIZE);
 EventQueue queue4(32 * EVENTS_EVENT_SIZE);
+
+int PredictGesture(float* output);
 int gesture_result();
+
 constexpr int kTensorArenaSize = 60 * 1024;
 uint8_t tensor_arena[kTensorArenaSize];
 
@@ -48,8 +54,8 @@ int player;
 int endplaying=0;
 int nowplaying=0;
 int nextsong;
-int numberofsongs=5;
-int ready_to_load=5;
+int numberofsongs=6;
+int ready_to_load=6;
 int song_select_mode=0;
 int load_or_unload_mode=0;
 int unload_mode=0;
@@ -59,12 +65,17 @@ int resetmusicplay=0;
 int f=1;
 int taikochoose=0;
 
+float song[signalLength];
+char serialInBuffer[bufferLength];
+int serialCount = 0;
+
 int16_t waveform[kAudioTxBufferSize];
 void playmusic(int);
+void songsplit();
 void loadsong();
-void load_or_unload();
-void taikodisplay(int);
-int gettaikolength(int );
+
+
+
 // Set up logging.
 static tflite::MicroErrorReporter micro_error_reporter;
 tflite::ErrorReporter* error_reporter = &micro_error_reporter;
@@ -150,7 +161,7 @@ void songs::unload(){
   //info=NULL;
 }
 
-songs songlist[5];
+songs songlist[6];
 
 void song_list(){
   uLCD.cls();
@@ -189,141 +200,71 @@ void song_list(){
   song_select_mode=0;
 
 }
-void load_or_unload(){
-  uLCD.cls();
-  load_or_unload_mode=1;
-  uLCD.printf("load: ring\n");
-  uLCD.printf("unload: slope\n");
-  int result=gesture_result();
-  load_or_unload_mode=0;
-  if(result==0)
-    loadsong();
-  else if(result==1){
-    uLCD.cls();
-    uLCD.textbackground_color(BLACK);
-    for(int i=0;i<numberofsongs;i++){
-      if(i==0)
-        uLCD.textbackground_color(BLUE);
-      else
-        uLCD.textbackground_color(BLACK);
-      uLCD.printf("%d:",i);
-      songlist[i].printname();
-      uLCD.printf("\n");
-    }
-    uLCD.printf("which song to unload\n");
-    uLCD.printf("forward :ring\n");
-    uLCD.printf("backward :slope\n");
-    unload_mode=1;
-    int unloadnumber=gesture_result();
-    unload_mode=0;
-    songlist[unloadnumber].unload();
-    int i=unloadnumber;
-    while(i<numberofsongs-1){
-      songlist[i]=songlist[i+1];
-      //songlist[i].length=songlist[i+1].length;
-      //songlist[i].name=songlist[i+1].name;
-      i++;
-    }
-    numberofsongs--;
-    ready_to_load--;
-  }
 
+void songsplit(){
+
+  int song1[84], song2[98], song3[94], song4[64], song5[76];
+  int j;
+  
+  
+  for(j=0; j<84; j++){
+    song1[j] = (int)(song[j]*1000);
+  }
+ 
+  for(j=84; j<182; j++){
+    song2[j-84] = (int)(song[j]*1000);
+  }
+ 
+  for(j=182; j<276; j++){
+    song3[j-182] = (int)(song[j]*1000);
+  }
+ 
+  for(j=276; j<340; j++){
+    song4[j-276] = (int)(song[j]*1000);
+  }
+ 
+  for(j=340; j<416; j++){
+    song5[j-340] = (int)(song[j]*1000);
+  }
+ 
+  songlist[0].loadinfo(song1);
+  songlist[1].loadinfo(song2);
+  songlist[2].loadinfo(song3);
+  songlist[3].loadinfo(song4);
+  songlist[4].loadinfo(song5);
+  
+  
 }
 
-void loadsong(){
+void loadsong()
+{
   
-  int i=0; 
-  char a[2];
-  char sp;
-  int count=0;
-  i=0;
-  while(1){
-    if(pc.getc()=='#')
-      break;
-  }
-
-  while(true){
-    if(pc.readable()){
-      a[i]=pc.getc();
-      count++;
-      if(count==2){
-        //a[count]='\0';
-        sp=pc.getc();
-        break;
-      }
-      i++;
-    }
-  }
-  
-  songlist[ready_to_load].speed=(int)sp -48;
-  /*for(int j=0;j<2;j++){
-    uLCD.printf("%c",a[j]);
-  }*/
-  int signalLength=(int)atof(a);
-  
-  songlist[ready_to_load].length=signalLength;
-  
-  i=0;
-  char read[20];
-  while(true){
-    if(pc.readable()){
-      read[i]=pc.getc();
-
-      if(read[i]=='$'){
-        read[i]='\0';
-        break;
-      }
-      i++;
-    }
-  }
-  songlist[ready_to_load].name=string(read);
-  int signal[2*songlist[ready_to_load].length];
-  int serialCount = 0;
-  char serialInBuffer[32];
-  i=0;  
-  while(i < signalLength){
-    if(pc.readable()){
+  int i = 0;
+  serialCount = 0;
+  audio.spk.pause();
+  while(i < signalLength)
+  {
+    if(pc.readable())
+    {
       serialInBuffer[serialCount] = pc.getc();
       serialCount++;
-      if(serialCount == 3 ){
+      if(serialCount == 5)
+      {
         serialInBuffer[serialCount] = '\0';
-        signal[i] = (int) atof(serialInBuffer);
+        song[i] = (int) atof(serialInBuffer);
         serialCount = 0;
         i++;
       }
     }
   }
-  i=0;
-  int j=signalLength;
-  serialCount = 0;
-  char in_noteL[100];
-  while(i < signalLength){
-    if(pc.readable()){
-      in_noteL[i] = pc.getc();
-      signal[j]= (int) in_noteL[i]-48;
-      i++;
-      j++;
-    }
-  }
   
-  uLCD.cls();
-  uLCD.printf("load done\n");
-  int k=0;
-  while(songlist[ready_to_load].name[k]!='\0'){
-    uLCD.printf("%c",songlist[ready_to_load].name[k]);
-    k++;
-  }
-  songlist[ready_to_load].loadinfo(signal);
-  uLCD.printf("%d",songlist[ready_to_load].speed);
-  ready_to_load++;
-  numberofsongs++;
+  songsplit();
+  
 }
 
 void mode_selection(){
   if(timers.read_ms()>1000){
-    //queue3.cancel(idC);
     queue2.cancel(player);
-    taikochoose=0;
     f=0;
     resetmusicplay=1;
     playNote(0);
@@ -415,7 +356,7 @@ void playmusic(int reset){
 
     }
   }
-  
+} 
 
 // Return the result of the last prediction
 int PredictGesture(float* output) {
@@ -457,160 +398,6 @@ int PredictGesture(float* output) {
   return this_predict;
 }
 
-int main(int argc, char* argv[]) {
-
-  uLCD.cls();
-  uLCD.color(GREEN);
-  uLCD.locate(0,5);
-  uLCD.printf("** User Manual **\n\n");
-  uLCD.printf(" Press SW2 to\n enter MODE SEL\n and following\n the instructions.\n");
-
-  t1.start(callback(&queue1, &EventQueue::dispatch_forever));
-  t2.start(callback(&queue2, &EventQueue::dispatch_forever));
-  t3.start(callback(&queue3, &EventQueue::dispatch_forever));
-  t4.start(callback(&queue4, &EventQueue::dispatch_forever));
-  timers.start();
-  button.rise(queue1.event(mode_selection));
-  songlist[0].name="little star";
-  songlist[1].name="little bee";
-  songlist[2].name="jingle bells";
-  songlist[3].name="two tigers";
-  songlist[4].name="train run";
-
-  int song1[84]={261, 261, 392, 392, 440, 440, 392,
-                349, 349, 330, 330, 294, 294, 261,
-                392, 392, 349, 349, 330, 330, 294,
-                392, 392, 349, 349, 330, 330, 294,
-                261, 261, 392, 392, 440, 440, 392,
-                349, 349, 330, 330, 294, 294, 261,
-                1, 1, 1, 1, 1, 1, 2,
-                1, 1, 1, 1, 1, 1, 2,
-                1, 1, 1, 1, 1, 1, 2,
-                1, 1, 1, 1, 1, 1, 2,
-                1, 1, 1, 1, 1, 1, 2,
-                1, 1, 1, 1, 1, 1, 2};
-  songlist[0].length=42;
-  songlist[0].loadinfo(song1);
-  songlist[0].speed=1;
-  
-  int song2[98]={
-    392,330,330,349,
-    294,294,261,294,330,349,392,392,392,392,330,330,349,
-    294,294,261,330,392,392,330,294,294,294,294,294,330,349,330,
-    330,330,330,330,349,392,392,330,330,349,294,294,261,330,392,392,261,
-    1,1,2,1,1,2,1,1,1,1,1,1,2,
-    1,1,2,1,1,2,1,1,1,1,4,
-    1,1,1,1,1,1,2,1,1,1,1,1,1,2,
-    1,1,2,1,1,2,1,1,1,1,4
-  };
-  songlist[1].length=49;
-  songlist[1].loadinfo(song2);
-  songlist[1].speed=1;
-
-  int song3[95]={
-    330,330,330,330,330,330,330,392,261,294,
-    330,349,349,349,349,349,330,330,330,294,294,330,294,
-    392,330,330,330,330,330,330,330,392,261,294,330,349,349,349,
-    349,349,330,330,392,392,349,294,261,
-    1,1,2,1,1,2,1,1,1,1,4,
-    1,1,2,1,1,1,1 ,1,1,1,1,2,2,
-    1,1,2,1,1,2,1,1,1,1,4,
-    1,1,2,1,1,1,1 ,1,1,1,1,2,2
-  };
-  songlist[2].length=47;
-  songlist[2].loadinfo(song3);
-  songlist[2].speed=1;
-
-  int song4[64]={
-    261,294,330,261,261,294,330,261,330,349,392,330,349,392,392,440,392,349,330,261,392,440,392,349,330,261,261,196,261,261,196,261,
-    2,2,2,2,2,2,2,2,
-    2,2,4,2,2,4,
-    1,1,1,1,2,2,1,1,1,1,2,2,
-    2,2,4,2,2,4
-  };
-  songlist[3].length=32;
-  songlist[3].loadinfo(song4);
-  songlist[3].speed=2;
-
-  int song5[76]={
-    392,392,330,261,392,392,330,261,294,330,349,349,330,349,349,392,392,330,392,330,294,330,261,349,294,294,294,330,261,261,261,294,330,349,294,261,247,261,
-    1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,2,
-    1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,2
-  };
-  songlist[4].length=38;
-  songlist[4].loadinfo(song5);
-  songlist[4].speed=1;
-
-  nextsong=0;
-  // wait(1);
-  
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
-    error_reporter->Report(
-        "Model provided is schema version %d not equal "
-        "to supported version %d.",
-        model->version(), TFLITE_SCHEMA_VERSION);
-    return -1;
-  }
-
-  // Pull in only the operation implementations we need.
-  // This relies on a complete list of all the ops needed by this graph.
-  // An easier approach is to just use the AllOpsResolver, but this will
-  // incur some penalty in code space for op implementations that are not
-  // needed by this graph.
-  static tflite::MicroOpResolver<6> micro_op_resolver;
-  micro_op_resolver.AddBuiltin(
-      tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
-      tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_MAX_POOL_2D,
-                               tflite::ops::micro::Register_MAX_POOL_2D());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
-                               tflite::ops::micro::Register_CONV_2D());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_FULLY_CONNECTED,
-                               tflite::ops::micro::Register_FULLY_CONNECTED());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_SOFTMAX,
-                               tflite::ops::micro::Register_SOFTMAX());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
-                             tflite::ops::micro::Register_RESHAPE(), 1);
-  // Build an interpreter to run the model with
-  static tflite::MicroInterpreter static_interpreter(
-      model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
- interpreter = &static_interpreter;
-
-  // Allocate memory from the tensor_arena for the model's tensors
-  interpreter->AllocateTensors();
-
-  // Obtain pointer to the model's input tensor
-  model_input = interpreter->input(0);
-  if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
-      (model_input->dims->data[1] != config.seq_length) ||
-      (model_input->dims->data[2] != kChannelNumber) ||
-      (model_input->type != kTfLiteFloat32)) {
-    error_reporter->Report("Bad input tensor parameters in model");
-    return -1;
-  }
-
- input_length = model_input->bytes / sizeof(float);
-
-  TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
-  if (setup_status != kTfLiteOk) {
-    error_reporter->Report("Set up failed\n");
-    return -1;
-  }
-
-  error_reporter->Report("Set up successful...\n");
-  
-  while(1){
-    
-    redled=1;
-    greenled=0;
-    wait(0.5);
-    
-  }
-  
-}
 int gesture_result(){
   // Create an area of memory to use for input, output, and intermediate arrays.
   // The size of this will depend on the model you're using, and may need to be
@@ -727,4 +514,190 @@ int gesture_result(){
   }
   else
     return getanswer;
+}
+
+int main(int argc, char* argv[]) {
+
+  uLCD.cls();
+  uLCD.color(GREEN);
+  uLCD.locate(0,5);
+  uLCD.printf("** User Manual **\n\n");
+  uLCD.printf(" Press SW2 to\n enter MODE SEL\n and following\n the instructions.\n");
+
+  t1.start(callback(&queue1, &EventQueue::dispatch_forever));
+  t2.start(callback(&queue2, &EventQueue::dispatch_forever));
+  t3.start(callback(&queue3, &EventQueue::dispatch_forever));
+  t4.start(callback(&queue4, &EventQueue::dispatch_forever));
+  timers.start();
+  button.rise(queue1.event(mode_selection));
+
+  songlist[0].name="little star";
+  songlist[1].name="little bee";
+  songlist[2].name="jingle bell";
+  songlist[3].name="two tigers";
+  songlist[4].name="train fly fast";
+  songlist[5].name="ode to joy";
+
+  int song1[84]={261, 261, 392, 392, 440, 440, 392,
+                349, 349, 330, 330, 294, 294, 261,
+                392, 392, 349, 349, 330, 330, 294,
+                392, 392, 349, 349, 330, 330, 294,
+                261, 261, 392, 392, 440, 440, 392,
+                349, 349, 330, 330, 294, 294, 261,
+                1, 1, 1, 1, 1, 1, 2,
+                1, 1, 1, 1, 1, 1, 2,
+                1, 1, 1, 1, 1, 1, 2,
+                1, 1, 1, 1, 1, 1, 2,
+                1, 1, 1, 1, 1, 1, 2,
+                1, 1, 1, 1, 1, 1, 2};
+  songlist[0].length=42;
+  songlist[0].loadinfo(song1);
+  songlist[0].speed=1;
+  
+  int song2[98]={
+    392,330,330,349,
+    294,294,261,294,330,349,392,392,392,392,330,330,349,
+    294,294,261,330,392,392,330,294,294,294,294,294,330,349,330,
+    330,330,330,330,349,392,392,330,330,349,294,294,261,330,392,392,261,
+    1,1,2,1,1,2,1,1,1,1,1,1,2,
+    1,1,2,1,1,2,1,1,1,1,4,
+    1,1,1,1,1,1,2,1,1,1,1,1,1,2,
+    1,1,2,1,1,2,1,1,1,1,4
+  };
+  songlist[1].length=49;
+  songlist[1].loadinfo(song2);
+  songlist[1].speed=1;
+
+  int song3[94]={
+    330,330,330,330,330,330,330,392,261,294,330,
+    349,349,349,349,349,330,330,330,294,294,330,294,392,
+    330,330,330,330,330,330,330,392,261,294,330,
+    349,349,349,349,349,330,330,392,392,349,294,261,
+    1,1,2,1,1,2,1,1,1,1,4,
+    1,1,2,1,1,1,1 ,1,1,1,1,2,2,
+    1,1,2,1,1,2,1,1,1,1,4,
+    1,1,2,1,1,1,1 ,1,1,1,2,2
+  };
+  songlist[2].length=47;
+  songlist[2].loadinfo(song3);
+  songlist[2].speed=1;
+
+  int song4[64]={
+    261,294,330,261,261,294,330,261,
+    330,349,392,330,349,392,
+    392,440,392,349,330,261,392,440,392,349,330,261,
+    261,196,261,261,196,261,
+    2,2,2,2,2,2,2,2,
+    2,2,4,2,2,4,
+    1,1,1,1,2,2,1,1,1,1,2,2,
+    2,2,4,2,2,4
+  };
+  songlist[3].length=32;
+  songlist[3].loadinfo(song4);
+  songlist[3].speed=2;
+
+  int song5[76]={
+    392,392,330,261,392,392,330,261,
+    294,330,349,349,330,349,349,392,
+    392,330,392,330,294,330,261,
+    349,294,294,294,330,261,261,261,
+    294,330,349,294,261,247,261,
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,2,
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,2
+  };
+  songlist[4].length=38;
+  songlist[4].loadinfo(song5);
+  songlist[4].speed=1;
+
+  int song6[124]={
+    330,330,349,392,392,349,330,294,
+    262,262,294,330,330,294,294,
+    330,330,349,392,392,349,330,294,
+    262,262,294,330,294,262,262,
+    294,294,330,262,294,330,349,330,262,
+    294,330,349,330,294,262,294,262,
+    330,330,349,392,392,349,330,294,
+    262,262,294,330,294,262,262,
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,2,1,2,
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,2,1,2,
+    1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,2,1,2,
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,2,1,2,
+  };
+  songlist[5].length=62;
+  songlist[5].loadinfo(song6);
+  songlist[5].speed=1;
+
+  nextsong=0;
+  // wait(1);
+  
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    error_reporter->Report(
+        "Model provided is schema version %d not equal "
+        "to supported version %d.",
+        model->version(), TFLITE_SCHEMA_VERSION);
+    return -1;
+  }
+
+  // Pull in only the operation implementations we need.
+  // This relies on a complete list of all the ops needed by this graph.
+  // An easier approach is to just use the AllOpsResolver, but this will
+  // incur some penalty in code space for op implementations that are not
+  // needed by this graph.
+  static tflite::MicroOpResolver<6> micro_op_resolver;
+  micro_op_resolver.AddBuiltin(
+      tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
+      tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_MAX_POOL_2D,
+                               tflite::ops::micro::Register_MAX_POOL_2D());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
+                               tflite::ops::micro::Register_CONV_2D());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_FULLY_CONNECTED,
+                               tflite::ops::micro::Register_FULLY_CONNECTED());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_SOFTMAX,
+                               tflite::ops::micro::Register_SOFTMAX());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
+                             tflite::ops::micro::Register_RESHAPE(), 1);
+  // Build an interpreter to run the model with
+  static tflite::MicroInterpreter static_interpreter(
+      model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+ interpreter = &static_interpreter;
+
+  // Allocate memory from the tensor_arena for the model's tensors
+  interpreter->AllocateTensors();
+
+  // Obtain pointer to the model's input tensor
+  model_input = interpreter->input(0);
+  if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
+      (model_input->dims->data[1] != config.seq_length) ||
+      (model_input->dims->data[2] != kChannelNumber) ||
+      (model_input->type != kTfLiteFloat32)) {
+    error_reporter->Report("Bad input tensor parameters in model");
+    return -1;
+  }
+
+ input_length = model_input->bytes / sizeof(float);
+
+  TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
+  if (setup_status != kTfLiteOk) {
+    error_reporter->Report("Set up failed\n");
+    return -1;
+  }
+
+  error_reporter->Report("Set up successful...\n");
+  
+  while(1){
+    
+    redled=1;
+    greenled=0;
+    wait(0.5);
+    
+  }
+  
 }
